11aa1ee535886ca7932625d7dc3117f2c118a1d7
JesusVicken <jesus.viccken@gmail.com>
2022-05-19 13:19:16 -0300

    commitando

---
+++ aulaif.py
@@ -0,0 +1,12 @@
+a = -9
+b = 4
+
+
+if a > b:
+    if b > 0:
+        print('A é maior que B \n é positivo')
+        print('b não é maior que a nem positivo')
+else:
+    print('b menor que a')
+
+
